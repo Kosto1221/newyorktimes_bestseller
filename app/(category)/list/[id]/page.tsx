a55572d672f3b48f8e8e9a2583d3a9dc1433e257
Kosto1221 <ostolany@gamil.com>
2024-11-17 19:16:12 +0900

    ????

--- app/(category)/list/[id]/page.tsx
+++ app/(category)/list/[id]/page.tsx
@@ -5,33 +5,31 @@ import MainContainer from "../../../../components/common/mainContainer";
 import Heading from "../../../../components/common/heading";
 
 interface IParams {
-  id: string;
+  params: { id: string };
 }
 
-export async function generateMetadata({ params }: { params: IParams }) {
-  const category = await getBooks(params.id);
+export async function generateMetadata({ params: { id } }: IParams) {
+  const category = await getBooks(id);
   return {
     title: category.results.list_name,
   };
 }
 
-export default async function ListByCategory(props: any) {
-  const params = props.params as IParams;
-  const booksData = await getBooks(params.id);
+export default async function ListByCategory({ params: { id } }: IParams) {
+  const booksData = await getBooks(id);
   const books = booksData.results.books;
-
   return (
     <MainContainer>
       <Heading type="h1" text={booksData.results.list_name} />
       <GridContainer>
-        {books.map((item: any) => (
+        {books.map((item) => (
           <BookContainer
             key={item.title}
             imagePath={item.book_image}
             title={item.title}
             author={item.author}
             link={item.amazon_product_url}
-          />
+          ></BookContainer>
         ))}
       </GridContainer>
     </MainContainer>
